0c10acf8990ed071f35dc950511b26bfa509aa27
Phil Chen <06fahchen@gmail.com>
2019-11-14 22:13:35 +0800

    format

--- lib/sonix.ex
+++ lib/sonix.ex
@@ -15,7 +15,7 @@ defmodule Sonix do
 
   """
 
-  def init(host \\ {127,0,0,1}, port \\ 1491) do
+  def init(host \\ {127, 0, 0, 1}, port \\ 1491) do
     {:ok, conn} = Tcp.start_link(host, port, [mode: :binary, packet: :line], 1000)
     Tcp.recv(conn)
     conn
@@ -34,12 +34,11 @@ defmodule Sonix do
     :ok = Tcp.send(conn, "START #{channel} #{password}")
     Tcp.recv(conn)
   end
-  
 
   @doc """
-  
+
   Query/Suggest a term
-  
+
   ## Parameters
 
     - opts: default values are [type: "QUERY", collection: "", bucket: "default", term: "", limit: 10, offset: 0]
@@ -48,29 +47,50 @@ defmodule Sonix do
 
       iex> Sonix.search(conn, [type: "QUERY", collection: "messages", "term": "spiderman"])
       obj:1
-      
+
       iex> Sonix.suggest(conn, [type: "SUGGEST", collection: "messages", "term": "spider"])
       spiderman
-      
+
   """
 
-  @default_search %{type: "QUERY", collection: "", bucket: "default", term: "", limit: 10, offset: 0}  
+  @default_search %{
+    type: "QUERY",
+    collection: "",
+    bucket: "default",
+    term: "",
+    limit: 10,
+    offset: 0
+  }
   def search(conn, opts \\ []) do
-    %{type: type, collection: collection, bucket: bucket, term: term, limit: limit, offset: offset} = Enum.into(opts, @default_search)
-    offset_str = if type == "QUERY", do: "OFFSET(#{offset})" ,else: ""
-    :ok = Tcp.send(conn, "#{type} #{collection} #{bucket} \"#{term}\" LIMIT(#{limit}) #{offset_str}")
+    %{
+      type: type,
+      collection: collection,
+      bucket: bucket,
+      term: term,
+      limit: limit,
+      offset: offset
+    } = Enum.into(opts, @default_search)
+
+    offset_str = if type == "QUERY", do: "OFFSET(#{offset})", else: ""
+
+    :ok =
+      Tcp.send(conn, "#{type} #{collection} #{bucket} \"#{term}\" LIMIT(#{limit}) #{offset_str}")
+
     p = Tcp.recv(conn)
-    "PENDING " <> pending_id =  p
+    "PENDING " <> pending_id = p
     x = Tcp.recv(conn)
     "EVENT " <> y = x
-    y |> String.replace("#{type} #{pending_id} ", "") |> String.split(" ") |> Enum.filter(& &1 != "")
+
+    y
+    |> String.replace("#{type} #{pending_id} ", "")
+    |> String.split(" ")
+    |> Enum.filter(&(&1 != ""))
   end
-  
 
   @doc """
-  
+
   Push a text
-  
+
   ## Parameters
 
     - opts: default values are [collection: "", bucket: "default", object: "", term: ""]
@@ -79,13 +99,16 @@ defmodule Sonix do
 
       iex> Sonix.push(conn, [collection: "messages", object: "obj:1", "term": "spiderman is cool"])
       :ok
-      
+
   """
 
-  @default_push %{collection: "", bucket: "default", object: "", term: ""}  
+  @default_push %{collection: "", bucket: "default", object: "", term: ""}
   def push(conn, opts \\ []) do
-    %{collection: collection, bucket: bucket, object: object, term: term} = Enum.into(opts, @default_push)
+    %{collection: collection, bucket: bucket, object: object, term: term} =
+      Enum.into(opts, @default_push)
+
     :ok = Tcp.send(conn, "PUSH #{collection} #{bucket} #{object} \"#{term}\" ")
+
     case Tcp.recv(conn) do
       "OK" -> :ok
       "ERR " <> reason -> {:err, reason}
@@ -93,9 +116,9 @@ defmodule Sonix do
   end
 
   @doc """
-  
+
   Pop a text
-  
+
   ## Parameters
 
     - opts: default values are [collection: "", bucket: "default", object: "", term: ""]
@@ -104,20 +127,22 @@ defmodule Sonix do
 
       iex> Sonix.pop(conn, [collection: "messages", object: "obj:1", "term": "spiderman"])
       1
-      
+
   """
 
   def pop(conn, opts \\ []) do
-    %{collection: collection, bucket: bucket, object: object, term: term} = Enum.into(opts, @default_push)
+    %{collection: collection, bucket: bucket, object: object, term: term} =
+      Enum.into(opts, @default_push)
+
     :ok = Tcp.send(conn, "POP #{collection} #{bucket} #{object} \"#{term}\" ")
     "RESULT " <> n = Tcp.recv(conn)
     n |> Integer.parse() |> elem(0)
   end
-  
+
   @doc """
-  
+
   Flush a collection, bucket or object
-  
+
   ## Parameters
 
     - opts: default values are [collection: "", bucket: "", object: ""]
@@ -126,18 +151,19 @@ defmodule Sonix do
 
       iex> Sonix.flush(conn, [collection: "messages"])
       1
-      
+
   """
 
-  @default_flush %{collection: "", bucket: "", object: ""}  
+  @default_flush %{collection: "", bucket: "", object: ""}
   def flush(conn, opts \\ []) do
     %{collection: collection, bucket: bucket, object: object} = Enum.into(opts, @default_flush)
 
-    cmd = cond do
-      object != "" -> "FLUSHO #{collection} #{bucket} #{object}"
-      bucket != "" -> "FLUSHB #{collection} #{bucket}"
-      collection != "" -> "FLUSHC #{collection}"
-    end
+    cmd =
+      cond do
+        object != "" -> "FLUSHO #{collection} #{bucket} #{object}"
+        bucket != "" -> "FLUSHB #{collection} #{bucket}"
+        collection != "" -> "FLUSHC #{collection}"
+      end
 
     :ok = Tcp.send(conn, cmd)
     "RESULT " <> n = Tcp.recv(conn)
@@ -145,9 +171,9 @@ defmodule Sonix do
   end
 
   @doc """
-  
+
   Count items in a collection, bucket or object
-  
+
   ## Parameters
 
     - opts: default values are [collection: "", bucket: "", object: ""]
@@ -156,17 +182,18 @@ defmodule Sonix do
 
       iex> Sonix.count(conn, [collection: "messages"])
       1
-      
+
   """
 
   def count(conn, opts \\ []) do
     %{collection: collection, bucket: bucket, object: object} = Enum.into(opts, @default_flush)
 
-    cmd = cond do
-      object != "" -> "COUNT #{collection} #{bucket} #{object}"
-      bucket != "" -> "COUNT #{collection} #{bucket}"
-      collection != "" -> "COUNT #{collection}"
-    end
+    cmd =
+      cond do
+        object != "" -> "COUNT #{collection} #{bucket} #{object}"
+        bucket != "" -> "COUNT #{collection} #{bucket}"
+        collection != "" -> "COUNT #{collection}"
+      end
 
     :ok = Tcp.send(conn, cmd)
     "RESULT " <> n = Tcp.recv(conn)
@@ -174,7 +201,7 @@ defmodule Sonix do
   end
 
   @doc """
-  
+
   Close a TCP connection
 
   ## Examples
@@ -185,7 +212,6 @@ defmodule Sonix do
 
   def quit(conn) do
     Tcp.send(conn, "QUIT")
-    Tcp.recv(conn)    
+    Tcp.recv(conn)
   end
-
 end
--- test/sonix_test.exs
+++ test/sonix_test.exs
@@ -1,6 +1,6 @@
 defmodule SonixTest do
   use ExUnit.Case, async: true
-  #doctest Sonix
+  # doctest Sonix
   require Logger
   alias Sonix.Tcp
 
@@ -10,8 +10,15 @@ defmodule SonixTest do
     conn = Sonix.init()
     Sonix.start(conn, "ingest", @pwd)
 
-    :ok = Sonix.push(conn, [collection: "messages", object: "obj:1", term: "Spiderman is bad movie"])
-    :ok = Sonix.push(conn, [collection: "messages", object: "obj:2", term: "Batman and spiderwoman is good Movie"])
+    :ok =
+      Sonix.push(conn, collection: "messages", object: "obj:1", term: "Spiderman is bad movie")
+
+    :ok =
+      Sonix.push(conn,
+        collection: "messages",
+        object: "obj:2",
+        term: "Batman and spiderwoman is good Movie"
+      )
 
     Sonix.quit(conn)
   end
@@ -19,52 +26,54 @@ defmodule SonixTest do
   def flush() do
     conn = Sonix.init()
     Sonix.start(conn, "ingest", @pwd)
-    _n = Sonix.flush(conn, [collection: "messages"])    
-    #Logger.info("Flushed #{n}")
+    _n = Sonix.flush(conn, collection: "messages")
+    # Logger.info("Flushed #{n}")
     Sonix.quit(conn)
   end
 
   test "PING PONG" do
     conn = Sonix.init()
     Sonix.start(conn, "search", @pwd)
-    
+
     Tcp.send(conn, "PING")
     y = Tcp.recv(conn)
-    
+
     Sonix.quit(conn)
 
     assert y == "PONG"
   end
+
   test "FLUSH ALL" do
     flush()
-    
+
     conn = Sonix.init()
     Sonix.start(conn, "ingest", @pwd)
-    x = Sonix.count(conn, [collection: "messages"])
+    x = Sonix.count(conn, collection: "messages")
 
     assert x == 0
     Sonix.quit(conn)
-
   end
+
   test "PUSH DATA" do
     ingest()
   end
+
   test "POP DATA" do
     flush()
     ingest()
 
     conn = Sonix.init()
     Sonix.start(conn, "ingest", @pwd)
-    x = Sonix.pop(conn, [collection: "messages", object: "obj:1", term: "spiderman"])
-    
+    x = Sonix.pop(conn, collection: "messages", object: "obj:1", term: "spiderman")
+
     assert x == 1
-    
-    x = Sonix.pop(conn, [collection: "messages", object: "obj:3", term: "noman"])
+
+    x = Sonix.pop(conn, collection: "messages", object: "obj:3", term: "noman")
     assert x == 0
 
     Sonix.quit(conn)
-
   end
+
   test "SEARCH DATA" do
     flush()
     ingest()
@@ -72,13 +81,14 @@ defmodule SonixTest do
     conn = Sonix.init()
     Sonix.start(conn, "search", @pwd)
 
-    x = Sonix.search(conn, [type: "QUERY", collection: "messages", term: "movie"])
-    assert x == ["obj:2","obj:1"]
-    x = Sonix.search(conn, [type: "QUERY", collection: "messages", term: "Spiderman"])
+    x = Sonix.search(conn, type: "QUERY", collection: "messages", term: "movie")
+    assert x == ["obj:2", "obj:1"]
+    x = Sonix.search(conn, type: "QUERY", collection: "messages", term: "Spiderman")
     assert x == ["obj:1"]
-    
+
     Sonix.quit(conn)
   end
+
   test "SEARCH INVALID DATA" do
     flush()
     ingest()
@@ -86,11 +96,12 @@ defmodule SonixTest do
     conn = Sonix.init()
     Sonix.start(conn, "search", @pwd)
 
-    x = Sonix.search(conn, [type: "QUERY", collection: "messages", term: "thisdoesnotexists"])
+    x = Sonix.search(conn, type: "QUERY", collection: "messages", term: "thisdoesnotexists")
     assert x == []
 
     Sonix.quit(conn)
-  end  
+  end
+
   test "ERROR HANDLE" do
     conn = Sonix.init()
     Sonix.start(conn, "search", @pwd)
@@ -99,23 +110,24 @@ defmodule SonixTest do
       :ok = Tcp.send(conn, "CAUSE AN ERROR")
       IO.inspect(Tcp.recv(conn))
     rescue
-      RuntimeError -> "" #Logger.info("Caught Error")
+      # Logger.info("Caught Error")
+      RuntimeError -> ""
     end
-
-  end 
-"""  
- Some issues SUGGEST isn't working with recently created data.
-  test "SUGGEST DATA" do
-    flush()
-    ingest()
-    
-    conn = Sonix.init()
-    Sonix.start(conn, "search", @pwd)
-    
-    x = Sonix.search(conn, [type: "SUGGEST", collection: "messages", term: "spi"])
-    assert x == ["spiderman", "spiderwoman"]
-
-    Sonix.quit(conn)
   end
-"""
+
+  """
+   Some issues SUGGEST isn't working with recently created data.
+    test "SUGGEST DATA" do
+      flush()
+      ingest()
+
+      conn = Sonix.init()
+      Sonix.start(conn, "search", @pwd)
+
+      x = Sonix.search(conn, [type: "SUGGEST", collection: "messages", term: "spi"])
+      assert x == ["spiderman", "spiderwoman"]
+
+      Sonix.quit(conn)
+    end
+  """
 end
